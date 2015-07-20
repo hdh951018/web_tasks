91301435d42586978c2d71e9877e23e5938a8819
JasonSi <529511542@qq.com>
2015-07-21 01:38:16 +0800

    Signed-off-by: JasonSi <529511542@qq.com>

--- B0/fib.rb
+++ B0/fib.rb
@@ -1,13 +1,17 @@
 f1,f2,f3=0,1,1
-puts "请输入一个1-100的数字"
-while (n=(STDIN.gets).to_i)<1
-puts "输入有误"
+puts "请输入一个1-100的数字,输入0直接退出"
+s=gets.chomp
+while((s.to_i.to_s != s)||(s.to_i<1)||(s.to_i>100))
+	exit if ((s.to_i == 0)&&(s.to_i.to_s == s)) #输入0直接退出
+	puts"请输入一个整数" if(s.to_i.to_s != s)
+	puts"超出范围，请重新输入" if(((s.to_i<1)||(s.to_i>100))&&(s.to_i.to_s == s))
+	s=gets.chomp
 end
-puts "1-#{n}的斐波那契数列如下"
-puts 1 if n==1
-while f3<n
+s=s.to_i
+#exit if s==0
+puts "1-#{s}的斐波那契数列如下"
+while f3 <= s
 puts f3
 f3=f2+f1
 f1,f2=f2,f3
 end
-
