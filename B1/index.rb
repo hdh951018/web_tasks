8a473aaa66e085e592fd84ed2c0af11207c9480f
JasonSi <529511542@qq.com>
2015-07-28 21:17:09 +0800

    调整界面展示，增添一些CSS样式，界面稍微美观一点，查询功能仍待增加，返回错误页面有待优化

--- B1/index.rb
+++ B1/index.rb
@@ -7,59 +7,64 @@ $idnum = 1                      #全局计数变量，在ADD函数里会自增
 $allMsg = Array.new
 
 #测试部分 预置内容
-for i in 1..5 do
-    MsgManage.add($allMsg, "第#{i}条留言内容 
-    	行走在冬夜的冷风中 飘散的 踩碎的 都是梦 孤单单这一刻 如何 确定你曾爱过我 停留在冬夜的冷风中 我不是 也不想 装脆弱 我没说不代表我不会痛
-	停留在冬夜的冷风中 我不是 也不想 装脆弱 只因为你说过 爱是等待是细水长流 Je le sais continue c’est pas bon   A la fin turestes pas longtemps
-	我没说不代表我不会痛","第#{i}位Jason")
+for i in 1..50 do
+  MsgManage.add($allMsg, "第#{i}条留言内容 
+  	行走在冬夜的冷风中 飘散的 踩碎的 都是梦 孤单单这一刻 如何 确定你曾爱过我 停留在冬夜的冷风中 我不是 也不想 装脆弱 我没说不代表我不会痛
+  	停留在冬夜的冷风中 我不是 也不想 装脆弱 只因为你说过 爱是等待是细水长流 Je le sais continue c’est pas bon   A la fin turestes pas longtemps
+  	我没说不代表我不会痛","第#{i}位Jason")
 end
-
-=begin
-    
-rescue Exception => e
-    
-end
-#测试
-MsgNeedEdit = MsgManage.searchByID($allMsg, 3)
-@originMsg = MsgNeedEdit.message
-p @originMsg
-=end
 
 get '/' do
-    @allMsg = $allMsg
-    erb :index
+  @allMsg = $allMsg
+#  erb :queryResult
+  erb :index
 end
 
-post '/' do
-    #筛选表单
-    erb :index
+post '/' do   #暂时没有实现
+  #筛选表单
+  @idQuery, @authorQuery = $allMsg
+  if params[:mode]=="id"
+    @idQuery = MsgManage.searchByID($allMsg, params[:query].to_i)
+  elsif params[:mode]=="author"
+    @authorQuery = MsgManage.searchByAuthor($allMsg, params[:query])
+  else
+    return "错误请求"    
+  end
 end
 
 get '/add' do 
-    erb :add
+  erb :add
 end
 
 post '/add' do
-    MsgManage.add($allMsg, params[:newContent], params[:newAuthor]) if params[:submit] == "提交"
-    redirect to ('/')
+  @newContent,@newAuthor = params[:newContent].lstrip.rstrip,params[:newAuthor].lstrip.rstrip
+  return redirect to ('/') if params[:addCncl] == '取消'
+  #判断字数是否符合要求
+  return erb :addFailed if (params[:newContent].lstrip.rstrip).length<10 || params[:newAuthor].lstrip.rstrip ==''
+  #点击提交之后，进行添加操作
+  MsgManage.add($allMsg, params[:newContent], params[:newAuthor]) if params[:addSub] == "提交"
+  redirect to ('/')
 end
 
 get '/delete/:idDelete'  do
-    @delResult = MsgManage.delete($allMsg, params[:idDelete].to_i)
-    erb :deleted
+  @delResult = MsgManage.delete($allMsg, params[:idDelete].to_i)
+  erb :deleted
 end
 
-get '/edit/:idEdit' do 
-    @allMsg = $allMsg
-    @idEdit = params[:idEdit]
-    return erb :editFailed   if MsgManage.searchByID($allMsg,params[:idEdit].to_i)=='NothingFound'
-    MsgNeedEdit = MsgManage.searchByID($allMsg, params[:idEdit].to_i)
-    @originMsg = MsgNeedEdit.message
-    @originAuthor = MsgNeedEdit.author
-    erb :edit
+get '/edit/:editID' do 
+  @allMsg = $allMsg
+  @editID = params[:editID]
+  return erb :editFailed  if MsgManage.searchByID($allMsg,params[:editID].to_i)=='NothingFound'
+  #如果没有找到该ID，直接渲染编辑失败的模板
+  MsgNeedEdit = MsgManage.searchByID($allMsg, params[:editID].to_i)
+  @originMsg = MsgNeedEdit.message
+  @originAuthor = MsgNeedEdit.author
+  erb :edit
 end
 
-post '/edit/:idEdit' do
-    MsgManage.edit($allMsg, params[:idEdit].to_i, params[:editContent], params[:editAuthor]) 
-    redirect to ('/')
+post '/edit/:editID' do
+  @newContent,@newAuthor = params[:editContent].lstrip.rstrip,params[:editAuthor].lstrip.rstrip
+  return erb :addFailed if (params[:editContent].lstrip.rstrip).length<10 || params[:editAuthor].lstrip.rstrip ==''
+  MsgManage.edit($allMsg, params[:editID].to_i, params[:editContent], params[:editAuthor]) if params[:editSub] == "确定"
+  redirect to ('/')
 end
