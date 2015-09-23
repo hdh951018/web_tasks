c4bd712cc579155832921b8e5dd6bbb670d970ca
JasonSi <529511542@qq.com>
2015-09-23 19:26:42 +0800

    重新修改CSS调用

--- B3/Blog/config/initializers/assets.rb
+++ B3/Blog/config/initializers/assets.rb
@@ -8,30 +8,23 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( kindeditor/kindeditor.js )
 
-Rails.application.config.assets.precompile += %w( Posts/style.css )
-Rails.application.config.assets.precompile += %w( Posts/bootstrap_new.css )
+# Rails.application.config.assets.precompile += %w( Posts/style.css )
+# Rails.application.config.assets.precompile += %w( Posts/bootstrap_new.css )
 
 
-# Rails.application.config.assets.precompile << Proc.new do |path|
-#   if path =~ /\.(css|js)\z/
-#     # if path.split('/')[-1] =~ /^_/
-#     #     false
-#     # else
-#     #     true
-#     # end
-#     full_path = Rails.application.assets.resolve(path).to_path
-#     app_assets_path = Rails.root.join('app', 'assets').to_path
-#     vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
-#     if full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path)
-#       # puts "including asset: " + full_path
-#       true
-#     else
-#       # puts "excluding asset: " + full_path
-#       false
-#     end
-#   else
-#     false
-#   end
-# end
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js|png)\z/
+    full_path = Rails.application.assets.resolve(path)
+    app_assets_path = Rails.root.join('app', 'assets').to_s
+    vendor_assets_path = Rails.root.join('vendor', 'assets').to_s
+    if full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path)
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
